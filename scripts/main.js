c5f99c798e3038cd517df16ed32a1fb0000fcd5e
wiola <w.kucharska99@gmail.com>
2020-05-23 18:05:49 +0200

    add dynamic selector

--- scripts/main.js
+++ scripts/main.js
@@ -4,30 +4,24 @@ const height = 600;
 const padding = 20;
 const margin = { top: 20, right: 0, bottom: 20, left: 30 };
 
-/*const languages = [
-    {id: 'FR', name: 'French'},
-    {id: 'GER', name: 'German'},
-    {id: 'IT', name: 'Italian'},
-    {id: 'RO', name: 'Romansh'},
-    {id: 'OTHER', name: 'Otherlanguages'},
-];*/
-
-
+let data = null;        // store the data and enable to reuse it
+let currentLanguage = 'FR';     // set language by default, the same as used un html
 
 function loadData() {
-	d3.dsv(',','data/principal_languages.csv').then((data) => {
-        setupPrincipalLanguages(data)
+	d3.dsv(',','data/principal_languages.csv').then((dataLoaded) => {
+        data = dataLoaded;  
+        drawGraph();
     });
 }
 
-function setupPrincipalLanguages(data){
+function drawGraph(){
+    d3.select('#languageGraph').remove();
 
-    console.log(data)
-    console.log(data)
     const svg = d3.select('.principalLanguages')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
+        .attr('id', 'languageGraph') // add id to find and remove svg
         .attr('style', 'font: 10px sans-serif');  
         
     // Horizontal scale
@@ -45,49 +39,35 @@ function setupPrincipalLanguages(data){
     svg.append('g')
         .attr('transform', `translate(60, 0)`)
         .call(d3.axisLeft(scaleY))
-        //remove lines in scales
-        //.call(g => g.select('.domain').remove());
         
-
     svg.append('g')
         .attr('transform', `translate(${margin.left}, ${height - margin.bottom})`)
         .call(d3.axisBottom(scaleX));
-            
-
-    const otherLanguagesData =   data.filter(d=> d.language === 'Otherlanguages');
-    //const frenchData =   data.filter(d=> d.language === 'French');
-    //const germanData =   data.filter(d=> d.language === 'German');
-    let currentLanguage = 'IT'
 
 
+    const filteredData = data.filter(d=> d.language === currentLanguage);
 
-//create color scale
-colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
-    .domain([0, d3.max(otherLanguagesData, d=> d.value)]);
+    //create color scale
+    colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
+        .domain([0, d3.max(filteredData, d=> d.value)]);
 
-
-//create bars 
+    //create bars 
     Bars = svg.append('g')
         .selectAll('rect')
-        .data(otherLanguagesData)
+        .data(filteredData)
         .join('rect')
         .attr('width', scaleX.bandwidth())
         .attr('height', d => scaleY(0) - scaleY(d.value))
         .attr('x', function(d) { return margin.left + scaleX(d.year) })
         .attr('y',  d => scaleY(d.value))
         .style('fill', d => colorScale(d.value));
-        // .attr('width', scaleX.bandwidth() - padding)
-        // .attr('height', d =>  scaleY(100 - d.value) - margin.bottom)
-        // .attr('x', d => margin.left + scaleX(d.year))
-        // .attr('y', d => scaleY(d.value))
-        // .style('fill', 'red');
 
-//Add text
+    //Add text
     Texte = svg.append('g')
         .style('fill', 'rgb(57,91,113)')
         .attr('text-anchor','end')
         .selectAll('text')
-        .data(otherLanguagesData)
+        .data(filteredData)
         .enter()
         .append('text')
         .attr('x', d => margin.left + scaleX(d.year) + (scaleX.bandwidth()/2) + 10 )
@@ -95,11 +75,12 @@ colorScale = d3.scaleSequential(d3.interpolateRdYlGn)
         .text(d => d.value);
 }
 
-
 function init() {
     loadData();
+    
+    d3.select('#language').on('change', function() {
+        currentLanguage = d3.event.target.value;
+        drawGraph()
+    });
 }
 init();
-
-//-----------------------------------------------------------------------------------------------------------------------
-
